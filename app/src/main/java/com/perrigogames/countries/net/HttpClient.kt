0bca38c6941077cae762648e491d6d9be47d8495
Corey Perrigo <corey@perrigogames.com>
2023-03-14 19:42:51 -0700

    Set up the HTTP client

---
+++ app/src/main/java/com/perrigogames/countries/net/HttpClient.kt
@@ -0,0 +1,18 @@
+package com.perrigogames.countries.net
+
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+internal val ktorHttpClient = HttpClient(Android) {
+
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            ignoreUnknownKeys = true
+        })
+    }
+}
